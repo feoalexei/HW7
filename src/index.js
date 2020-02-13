0da5c66f54284d8d02691f36bc220fdcffb4f736
feoalexei <feoalexei@ukr.net>
2020-02-13 20:15:47 +0200

    checked index.js by ES Lint

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-let inputString = prompt('Enter the original string:');
+const inputString = prompt('Enter the original string:');
 let inputStrLength;
 
-do { 
-    inputStrLength = +prompt('Enter the string length you need:', 0);
+do {
+    inputStrLength = +prompt('Enter the string length you need:', '0');
 } while (isNaN(inputStrLength));
 
-let inputSymbol = prompt('Enter the symbol to fill the string:');
-let inputFlag = prompt('Enter the side where the string should be filled (\'start\' or \'end\')');
-   
+const inputSymbol = prompt('Enter the symbol to fill the string:');
+const inputFlag = prompt('Enter the side where the string should be filled (\'start\' or \'end\')');
+
 function padString(string, targetLength, symbol, flag = 'end') {
     let symbolString = '';
     let newString;
-            
+
     for (let i = 0; i < (targetLength - string.length); i++) {
         symbolString += symbol;
     }
-        
+
     if (string.length < targetLength) {
         if (flag === 'end') {
-            newString = string + symbolString.slice(0, targetLength - string.length);
+            newString = string + symbolString.slice(0, targetLength - string.length); // eslint-disable-line
         } else if (flag === 'start') {
             newString = symbolString.slice(-(targetLength - string.length)) + string;
         } else {
@@ -30,6 +30,6 @@ function padString(string, targetLength, symbol, flag = 'end') {
         return string;
     }
 }
-        
-result = padString(inputString, inputStrLength, inputSymbol, inputFlag);
-console.log(result);
+
+const result = padString(inputString, inputStrLength, inputSymbol, inputFlag);
+console.log(result); // eslint-disable-line
